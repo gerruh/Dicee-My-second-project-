aebcc355911fe9696426186ed8e642a2d959aa7d
gerruh <avvewyccstrila.@gmail.com>
2022-06-26 23:22:51 +0300

    Just found how to use imageLiteral in Xcode 13, continuing with my education :)

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonGotPressed(_ sender: UIButton) {
+        /*diceImageView1.image = UIImage(named:"DiceFour")
+        diceImageView2.image = UIImage(named:"DiceFour")*/
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDiceNumber]//[созданный массив] [вызываемый привязанной переменной элемент массива равный значению переменной]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [rightDiceNumber]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+    
 }
 
